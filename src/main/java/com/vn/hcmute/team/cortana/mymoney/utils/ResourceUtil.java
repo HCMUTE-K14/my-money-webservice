8c96b343d93a6b9e776d0f2fbb38b6980d2ac54d
infamouS <daohuuloc9419@gmail.com>
2017-08-17 11:55:22 +0700

    fix

--- src/main/java/com/vn/hcmute/team/cortana/mymoney/utils/ResourceUtil.java
+++ src/main/java/com/vn/hcmute/team/cortana/mymoney/utils/ResourceUtil.java
@@ -1,6 +1,7 @@
 package com.vn.hcmute.team.cortana.mymoney.utils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -11,21 +12,31 @@ import org.apache.commons.logging.LogFactory;
 public class ResourceUtil {
 	public static final Log LOG=LogFactory.getLog(ResourceUtil.class);
 	
+	public static InputStream getResource(String path) throws FileNotFoundException{
+		return new FileInputStream(path); 
+	}
+	
 	public static Properties getConfigProperties(){
 		String path=ResourceUtil.class.getClassLoader().getResource("application.properties").getPath();
 		
 		Properties properties = null;
 
 		try {
-
-			InputStream input = new FileInputStream(path);
-
 			properties=new Properties();
-			properties.load(input);
+			properties.load(getResource(path));
 		} catch (IOException e) {
 			LOG.info("Cannot find file");
 		}
 
 		return properties;
 	}
+	public static InputStream getDefaultCategory(){
+		String path=ResourceUtil.class.getClassLoader().getResource("category_default.json").getPath();
+		try {
+			return getResource(path);
+		} catch (FileNotFoundException e) {
+			throw new NullPointerException("Cannot get file");
+		}
+	}
+	
 }
